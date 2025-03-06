a269e76be6974ed43ac65b438ed6863d5499e1b2
Esteban Bennett <estebanBennettb@gmail.com>
2025-03-06 20:17:32 +0100

    chore: base code

---
+++ src/main/java/com/examen_ad/academia/AcademiaApplication.java
@@ -0,0 +1,13 @@
+package com.examen_ad.academia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examen_ad/academia/AcademiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen_ad.academia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
